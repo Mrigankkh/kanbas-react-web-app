3379f5db378786336bfdf837809a606217387214
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-09-19 19:11:56 -0400

    Made the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import "./App.css";
 import { HashRouter, Route, Routes , Navigate} from "react-router-dom";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
+import Landing from "./Landing"
 function App() {
   return (
     <HashRouter>
       <div className="App">
         <Routes>
-        <Route path="/" element={<Navigate to="Kanbas" />} />
+        <Route path="/" element={<Landing />} />
 
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
---
+++ src/Landing/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Mrigank Khandelwal </h1>
+      <h2>CS4550 02 FA24</h2>
+    <br/>
+    <h2>Lab Assignments</h2>
+    <ul>
+      <li>
+        <Link to="/Labs/Lab1">Lab 1</Link>
+      </li>
+      <li>
+        <Link to="/Labs/Lab2">Lab 2</Link>
+      </li>
+      <li>
+        <Link to="/Labs/Lab3">Lab 3</Link>
+      </li>
+      </ul>
+
+      <h2>Link to Kanbas</h2>
+      <Link to="/Kanbas">Kanbas</Link>
+      <h2>Link to Guthub Repository</h2>
+      <a href="https://github.com/Mrigankkh/kanbas-react-web-app">Kanbas React Web App </a>
+    </div>
+  );
+};
+
+export default Landing; 
