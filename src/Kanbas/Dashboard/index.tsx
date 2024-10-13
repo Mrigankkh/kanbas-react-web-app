9c351b57d2820cd92344b809da8b65ef7a4a9fe0
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-10-13 18:04:06 -0400

    Starting a4; rebaseing on a3

---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import * as db from "../Database";
+import React, { useState } from "react";
+
+export default function Dashboard() {
+    const [courses, setCourses] = useState(db.courses);
+    return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title" className="float-start">Dashboard</h1> <br/><br/><hr />
+      <h2 id="wd-dashboard-published" className="float-start">
+
+        Published Courses ({courses.length})
+      </h2>{" "}
+      <br/><br/>
+      <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+              <div className="card rounded-3 overflow-hidden">
+                <Link
+                  to={`/Kanbas/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <img src={course.imageurl} width='auto' style={{maxWidth:'100%'}} height={160} />
+                  <div className="card-body">
+                    <h5 className="wd-dashboard-course-title card-title">
+                      {course.name}
+                    </h5>
+                    <p
+                      className="wd-dashboard-course-title card-text overflow-y-hidden"
+                      style={{ maxHeight: 100 }}
+                    >
+                      {course.description}
+                    </p>
+                    <button className="btn btn-primary float-start"> Go </button>
+                    <br/>
+                    <br/>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,8 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
+  console.log('Hello World!');
+
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
@@ -12,6 +14,8 @@ export default function Labs() {
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-      </Routes>    </div>
-);}
+        <Route path="Lab3/*" element={<Lab3 />} />
+      </Routes>{" "}
+    </div>
+  );
+}
