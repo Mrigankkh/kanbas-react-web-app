2965209d02b076f3d642167c442e809e91f33abf
Mrigank Khandelwal <mrigankkhandelwal300@gmail.com>
2024-10-17 21:35:42 -0400

    edits

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -28,11 +28,15 @@ import Styles from "./Styles";
 import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants />
+      <VariableTypes/>
+      <BooleanVariables />
       <IfElse />
       <TernaryOperator />
       <ConditionalOutputIfElse />
